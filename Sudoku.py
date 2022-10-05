52f7286ace4153c74f4fae556659073c6ab7e1da
JDCrawf <JDCrawford100@gmail.com>
2022-10-05 09:37:04 -0700

    small tweaks and global vars

--- Sudoku.py
+++ Sudoku.py
@@ -1,48 +1,72 @@
-grid = [[1,2,3,4,5,6,7,8,9],
-		[4,5,6,7,8,9,1,2,3],
-		[7,8,9,1,2,3,4,5,6],
-		[2,3,1,5,6,4,8,9,7],
-		[5,6,4,8,9,7,2,3,1],
-		[8,9,7,2,3,1,5,6,4],
-		[3,1,2,6,4,5,9,7,8],
-		[1,4,5,9,7,8,3,1,2],
-		[9,7,8,3,1,2,6,4,5]]
+from pickle import FALSE
+
+
+curs_pos_x = 0
+curs_pos_y = 0
+win_state = FALSE
+good_grid =	[[1,2,3,4,5,6,7,8,9],
+			[4,5,6,7,8,9,1,2,3],
+			[7,8,9,1,2,3,4,5,6],
+			[2,3,1,5,6,4,8,9,7],
+			[5,6,4,8,9,7,2,3,1],
+			[8,9,7,2,3,1,5,6,4],
+			[3,1,2,6,4,5,9,7,8],
+			[6,4,5,9,7,8,3,1,2],
+			[9,7,8,3,1,2,6,4,5]]
+
+bad_grid =	[[1,2,3,4,5,6,7,8,9],
+			[4,5,6,7,8,9,1,2,3],
+			[7,8,9,1,2,3,4,5,6],
+			[2,3,1,5,6,4,8,9,7],
+			[5,6,4,8,9,7,2,3,1],
+			[8,9,7,2,3,1,5,6,4],
+			[3,1,2,6,4,5,9,7,8],
+			[1,4,5,9,7,8,3,1,2],
+			[9,7,8,3,1,2,6,4,5]]
+
 
 # parses through sudoku grid checking for invalid lines/grids
 # first checks horizontally, then vertically, then by 3x3
-def SudokuCheck(grid):
+def SudokuCheck(_grid):
 	for i in range(9):
 		for j in range(9):
-			temp = abs(grid[i][j])
+			temp = abs(_grid[i][j])
 			# horizontal check
 			for k in range(9-j):
-				if (temp == abs(grid[i][j+k]) and k!=0):
-					grid[i][j] = -1 * abs(grid[i][j])
-					grid[i][j+k] = -1 * abs(grid[i][j])
+				if (temp == abs(_grid[i][j+k]) and k!=0):
+					_grid[i][j] = -1 * abs(_grid[i][j])
+					_grid[i][j+k] = -1 * abs(_grid[i][j])
 			# vertical check
 			for k in range(9-i):
-				if (temp == abs(grid[i+k][j]) and k!=0):
-					grid[i][j] = -1 * abs(grid[i][j])
-					grid[i+k][j] = -1 * abs(grid[i][j])
+				if (temp == abs(_grid[i+k][j]) and k!=0):
+					_grid[i][j] = -1 * abs(_grid[i][j])
+					_grid[i+k][j] = -1 * abs(_grid[i][j])
 			# 3x3 check
 			igrid = (int)(i/3)
 			jgrid = (int)(j/3)
 			for ig in range(igrid*3,igrid*3+3):
 				for jg in range(jgrid*3, jgrid*3+3):
-					if (temp == abs(grid[ig][jg]) and (i != ig and j != jg)):
-						grid[ig][jg] = -1 * abs(grid[i][j])
-						grid[i][j] = -1 * abs(grid[i][j])
+					if (temp == abs(_grid[ig][jg]) and (i != ig and j != jg)):
+						_grid[ig][jg] = -1 * abs(_grid[i][j])
+						_grid[i][j] = -1 * abs(_grid[i][j])
 
 # Prints out sudoku board
-# invalid lines have problems highlighted in red
-def SudokuPrint(grid):
-	SudokuCheck(grid)
+# invalid lines have issues highlighted in red
+def SudokuPrint(_grid):
+	SudokuCheck(_grid)
+	global curs_pos_x, curs_pos_y
 	for i in range(9):
 		for j in range(9):
-			if grid[i][j] < 0:
-				print('\033[91m' + str(abs(grid[i][j])) + '\033[0m', end = '')
-			else:
-				print(grid[i][j], end = '')
+			curr = str(abs(_grid[i][j]))
+			# if position is invalid change font to red
+			# Sudoku check makes invalid entries negative their entered number
+			if _grid[i][j] < 0:
+				curr = '\033[91m' + curr + '\033[0m'
+			if (curs_pos_x == i) and (curs_pos_y == j):
+				curr = '\033[7m' + curr + '\033[0m'
+			print(curr, end = '')
+
+			# print grid borders to group into 3X3 grids
 			if j != 8:
 				if (j+1) % 3 == 0:
 					print(' | ', end = '')
@@ -52,7 +76,8 @@ def SudokuPrint(grid):
 				print()
 		if i != 8:
 			if (i+1) % 3 == 0:
-				print('-----------------------------')
+				print('--------+---------+--------')
+	print('')
 
-
-SudokuPrint(grid)
+#SudokuPrint(good_grid)
+SudokuPrint(bad_grid, curs_pos_x, curs_pos_y)
